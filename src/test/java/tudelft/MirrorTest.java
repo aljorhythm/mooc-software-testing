f5480e1f2b6f25b79ee4a708781c43b9569218df
Joel Lim <103879u@gmail.com>
2019-11-18 20:33:38 +0800

    ghappy

--- src/test/java/tudelft/MirrorTest.java
+++ src/test/java/tudelft/MirrorTest.java
@@ -2,7 +2,7 @@ package tudelft;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MirrorTest {
     public String mirrorEnds(String string) {
@@ -25,5 +25,32 @@ public class MirrorTest {
         assertEquals("a", mirrorEnds("abca"));
         assertEquals("aba", mirrorEnds("aba"));
         assertEquals("abba", mirrorEnds("abba"));
+    }
+
+
+    public boolean gHappy(String str) {
+        assert str != null;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g') {
+                if (i > 0 && str.charAt(i - 1) == 'g') {
+                    continue;
+                }
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'g') {
+                    continue;
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void testGHappy() throws Exception {
+        assertTrue(gHappy("xxggxx"));
+        assertTrue(gHappy("xxggxx"));
+        assertFalse(gHappy("xxggyygxx"));
+        assertFalse(gHappy("xxggyygxx"));
+        assertTrue(gHappy("gg"));
+        assertFalse(gHappy("g"));
     }
 } 
