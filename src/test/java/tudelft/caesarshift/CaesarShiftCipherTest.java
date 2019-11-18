fc7a88889ec23b39356e6dfa089707b885c03ee0
Joel Lim <103879u@gmail.com>
2019-11-18 20:40:19 +0800

    caesar shift

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,21 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static tudelft.caesarshift.CaesarShiftCipher.caesarShiftCipher;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    void positive() {
+        assertEquals("def", caesarShiftCipher("abc", 3));
+        assertEquals("abc", caesarShiftCipher("xyz", 3));
+    }
+
+    @Test
+    void negative() {
+        assertEquals("abc", caesarShiftCipher("def", -3));
+        assertEquals("xyz", caesarShiftCipher("abc", -3));
+    }
 }
